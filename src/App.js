6bccdde4b77a92c1e53950eb704e588ee09d7e9d
Anubhav0103 <62379559+Anubhav0103@users.noreply.github.com>
2025-04-07 21:58:25 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga4';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -9,25 +8,10 @@ import Experience from './pages/Experience';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
-// Initialize Google Analytics 4 with the Measurement ID
-const TRACKING_ID = 'G-K1ZPE2XFXK'; // Your Measurement ID
-ReactGA.initialize(TRACKING_ID);
-
-// Component to track page views on route changes
-function TrackPageViews() {
-  const location = useLocation();
-  useEffect(() => {
-    // Send pageview with the current path
-    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
-  }, [location]);
-  return null;
-}
-
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-800 text-gray-300 font-sans">
-        <TrackPageViews />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
